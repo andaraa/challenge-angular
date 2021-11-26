967cd3b9621e75066475d7e644279e5ebf9f3159
andaraa <andikapradana140203@gmail.com>
2021-11-26 09:15:12 +0700

    data-bind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,15 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'y';
   number = 1;
+  itemImageUrl =
+    "https://akcdn.detik.net.id/visual/2020/07/02/toyota-alphard_169.png?w=650'";
+  serverName = '';
+  onUpdateServerName(event: any): any {
+    console.log(event); // dilihat dulu hasil returnnya
+    this.serverName = (<HTMLInputElement>event.target).value;
+    // HTMLInputElement ini interface dari event.target
+    // atau bisa juga dengan penulisan (event.target as HTMLInputElement)
+  }
   change(): any {
     this.number++;
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SuccesAlertComponent } from './succes-alert/succes-alert.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { Comp1Component } from './comp/comp1/comp1.component';
+import { Comp2Component } from './comp/comp2/comp2.component';
 
 @NgModule({
-  declarations: [AppComponent, SuccesAlertComponent, WarningAlertComponent],
+  declarations: [
+    AppComponent,
+    SuccesAlertComponent,
+    WarningAlertComponent,
+    Comp1Component,
+    Comp2Component,
+  ],
+
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
